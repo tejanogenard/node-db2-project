e5275dc82d908a94006cccd3f01b91b1ed1e8b37
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-06-10 03:52:26 -0700

    endpoints added

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -8,4 +8,33 @@ router.get("/", (req,res) => {
     res.json({message: "hello from the server!! "})
 })
 
+router.get('/', (req, res) => {
+    db('cars')
+    .then(cars => {
+      res.json(cars); 
+    })
+    .catch (err => {
+      res.status(500).json({ message: 'Failed to retrieve cars' });
+    });
+  });
+
+
+  router.post('/', (req, res) => {
+    const carData = req.body;
+    db('fruits').insert(carData)
+    .then(ids => {
+      db('cars').where({ id: ids[0] })
+      .then(newCarEntry => {
+        res.status(201).json(newCarEntry);
+      });
+    })
+    .catch (err => {
+      console.log('POST error', err);
+      res.status(500).json({ message: "Failed to store data" });
+    });
+  });
+
+
+  
+
 module.exports = router
